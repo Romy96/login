1a99473def6afc92881547b8e6ff40f1ddf0d796
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-04-03 11:18:07 +0200

    Home pagina gemaakt

---
+++ controller/ErrorController.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!function_exists(index())) {
+	function index()
+	{
+		echo "Uhmm, durp, een error! 404 ofzo?";
+	}
+}
+
---
+++ view/templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Hospital</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="<?= URL ?>css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="<?= URL ?>css/bootstrap-theme.min.css">
+
+<link rel="stylesheet" href="<?= URL ?>css/main.css">
+<!-- Latest compiled and minified JavaScript -->
+</head>
+<body>
+
+<div class="topbar">
+	<span class="" style="float:right;"><a class="navbar_link" href="register.php">Registreren</a></span>
+	<span class="" style="float:right;"><a class="navbar_link" href="login.php">Inloggen</a></span>
+</div>
